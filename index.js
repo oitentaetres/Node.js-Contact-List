5dcd168777b16057445fad028d71b34905da9058
Ivan Kuhne <oitentaetres@gmail.com>
2023-07-04 16:24:22 -0300

    Adjusts of search results

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var app = express();
 
 // Retrieving the data from the JSON file:
 
-var data = require("./data/mockContacts.json");
+var data = require("./data/mockContacts2.json");
 
 // Putting the data in alphabetical order:
 
 data = [...data].sort((a, b) => a.name.localeCompare(b.name));
 
-var newData = undefined;
+var newData = null;
+var name = null;
 
 const port = 3001;
 
@@ -17,15 +18,14 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/scripts"));
 
 app.get("/", function(req, res) {
-
-	var name = undefined;
 	
 	if (typeof req.query.name !== "undefined") {  
-		let name = req.query.name;
+		name = req.query.name;
 		newData = data.filter(d => d.name.toLowerCase().includes(name.toLowerCase()));
 	}
   
 	else {
+		name = null;
 		newData = data;
 	}
   
